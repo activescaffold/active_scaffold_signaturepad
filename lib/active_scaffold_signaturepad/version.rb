396fc426b925d5d1f79d634c265486e91b5bba66
Sergio Cambra <sergio@enpijama.es>
2024-08-21 09:57:33 +0200

    bump to 0.2.0

--- lib/active_scaffold_signaturepad/version.rb
+++ lib/active_scaffold_signaturepad/version.rb
@@ -1,8 +1,8 @@
 module ActiveScaffoldSignaturepad
   module Version
     MAJOR = 0
-    MINOR = 1
-    PATCH = 2
+    MINOR = 2
+    PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
